20c88a778a75b43e88d3d6edbd0707867af01a5b
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-10-23 12:07:18 +0100

    Añadiendo test de paginas de usuario

--- test/test.rb
+++ test/test.rb
@@ -12,12 +12,12 @@ def app
   Sinatra::Application
 end
 
-describe "Tests de la pagina raiz ('/') con metodo get" do
+describe "Tests de la pagina raiz principal ('/') con metodo get" do
 	before :all do
-		@imagen="/logo/logo.png"
+		@imagen="/public/logo/logo.png"
 		@textoTitulo="<title>Inicio</title>"
-		@textoCabecera="Twitter Most Popular Friend"
-		@textoContenido="NºAmigos a mostrar (max 10)"
+		@textoCabecera="Practica URL Shortener"
+		@textoContenido="Acorta tu URL"
 		@css="/public/css/lavish-bootstrap.css"
 	end
 
@@ -30,7 +30,7 @@ describe "Tests de la pagina raiz ('/') con metodo get" do
 		assert last_response.body.include?(@textoTitulo), "El titulo tiene que ser: "+@textoTitulo
 	end
 
-	it "Comprueba que en la pagina hay una cabecera Most Popular Twitter Friend" do
+	it "Comprueba que en la pagina hay una cabecera" do
 
 		get '/'
 		assert last_response.body.include?(@textoCabecera), "El titulo de cabecera tiene que estar en el contenido"
@@ -41,16 +41,15 @@ describe "Tests de la pagina raiz ('/') con metodo get" do
 		get '/'
 		assert last_response.body.include?(@textoContenido), "El contenido tiene que estar en la web"
 	end
-
-	it "Comprueba el mensaje en el campo de visualizacion de amigos" do
-		get '/'
-		assert last_response.body.include?("No se ha introducido ningun usuario o es incorrecto"), "Tiene que haber un mensaje informativo"
-
-	end	
 	
-	it "Comprueba la carga del logo de la web" do
-		get '/'
-		assert last_response.body.include?("/icons/"+@imagen), "Debe tener el logo de la web" 
+	it "Comprueba la carga del logo en el public del servidor" do
+		path = File.absolute_path(__FILE__)
+		path=path+@imagen
+		path=path.split('/test/test.rb')
+		path=path[0]+path[1]
+
+		assert File.exists?(path), "Debe estar la imagen en public"
+
 	end
 
 	it "Comprueba si esta el CSS en el servidor" do
@@ -64,3 +63,38 @@ describe "Tests de la pagina raiz ('/') con metodo get" do
 	end
 
 end
+
+describe "Test de la paginas paginas de login" do
+	before :all do
+		@pagina='/usr/google'
+		@pagina2='/usr/twitter'
+		@pagina3='/usr/facebook'		
+	end
+
+
+	it "Entrar en la pagina #{@pagina} sin loguearse" do	
+
+		get @pagina
+		respuesta=last_response.ok?
+		assert(not(respuesta))
+		
+	end
+	
+	it "Entrar en la pagina #{@pagina2} sin loguearse" do	
+
+		get @pagina2
+		respuesta=last_response.ok?
+		assert(not(respuesta))
+		
+	end
+
+	it "Entrar en la pagina #{@pagina3} sin loguearse" do	
+
+		get @pagina3
+		respuesta=last_response.ok?
+		assert(not(respuesta))
+		
+	end
+	
+end
+
